91b6d0338cc5bb111733ea7b6212f3036886db2a
Cheesenaught <ethandan2007@gmail.com>
2023-11-17 09:42:33 +0000

    oihfihgvoirdhfiguvbdifjkbv ijudfhbviu

--- users.php
+++ users.php
@@ -6,30 +6,17 @@
     
 </head>
 <body>
-    <form action="addusers.php" method = "post">
-    First name:<input type="text" name="forename"><br>
-    Password:<input type="passwo" name="passwd"><br>
-    Last name:<input type="text" name="surname"><br>
-    House:<input type="text" name="house"><br>
-    Year:<input type="text" name="year"><br>
-    <!--Creates a drop down list-->
-    Gender:<select name="gender">
-            <option value="M">Male</option>
-            <option value="F">Female</option>
-        </select>
-    <br>
-    <input type="submit" value="Add User">
-    </form>
-</body>
-</html>
-<?php
-include_once('connection.php');
-$stmt = $conn->prepare("SELECT * FROM TblUsers");
-$stmt->execute();
-
-while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
-{
-echo($row["Forename"].' '.$row["Surname"]."<br>");
-}
-
-?>
+<form action="addusers.php">
+  First name:<input type="text" name="forename"><br>
+  Last name:<input type="text" name="surname"><br>
+  Password:<input type="password" name="passwd"><br>
+  House:<input type="text" name="house"><br>
+  Year:<input type="text" name="year"><br>
+  <!--Creates a drop down list-->
+  Gender:<select name="gender">
+		<option value="M">Male</option>
+		<option value="F">Female</option>
+	</select>
+  <br>
+  <input type="submit" value="Add User">
+</form>
